f3133cdd9da574e4b1bbe3d01a57e9cbb33eecce
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:32:54 +0300

    Add project files.

---
+++ alphabeticFunctions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alphabeticFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Berkay";
+            string surname = "      Karaduman";
+            Console.WriteLine(name.ToLower() + " " + surname.ToUpper());
+            Console.WriteLine(name.StartsWith("B") + " " + surname.EndsWith("K"));
+            Console.WriteLine(name.IndexOf("B") + " " + surname.Insert(0,"Berkay"));
+            Console.WriteLine(name.Remove(3, 1) + " " + surname.Substring(8, 5));
+            Console.WriteLine(name.Remove(3) + " " + surname.Substring(6, 4));
+            Console.WriteLine(name.Replace("kay","kara") + " " + surname.Trim());
+            Console.WriteLine(name.Length + " " + surname.Length);
+        }
+    }
+}
